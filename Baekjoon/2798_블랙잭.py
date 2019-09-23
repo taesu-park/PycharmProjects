30fd8418821b5ec9f3d85470d8ac45432c9f9b39
taesu-park <tsoo1014@gmail.com>
2019-09-23 17:51:15 +0900

    190923 | solve blackjack!! | ssafy

--- Baekjoon/2798_블랙잭.py
+++ Baekjoon/2798_블랙잭.py
@@ -1,26 +1,30 @@
-N, M = map(int, input().split())
-arr = list(map(int, input().split()))
+import sys
+sys.setrecursionlimit(100000)
+
+N, M = map(int,input().split())
+arr = list(map(int,input().split()))
 result = 0
 a = 0
 sum_max = 0
-
-
 def back(n, k):
     global a, sum_max, result
     if a == 1:
         return
-    if k == 3 and result < M and result > sum_max:
-        sum_max = result
-        return
     if k == 3:
+        if result < M and result > sum_max:
+            sum_max = result
         return
+
     for i in range(n, N):
         result += arr[i]
-        if result == M:
+        if result == M and k == 2:
             a = 1
             print(result)
-        back(i + 1, k + 1)
+        back(i+1, k+1)
+        if a == 1:
+            break
         result -= arr[i]
+
 
 back(0, 0)
 if a == 0:
