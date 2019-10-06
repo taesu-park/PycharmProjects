65ec14151efc61abc555d8023b9456219faf34d4
taesu-park <tsoo1014@gmail.com>
2019-10-07 08:49:57 +0900

    solve

--- swexpert/D4/창용 마을 무리의 개수.py
+++ swexpert/D4/창용 마을 무리의 개수.py
@@ -1,9 +1,19 @@
+def DFS(x):
+    global visit, count
+    visit[x] = 1
+    for w in G[x]:
+        if not visit[w]:
+            DFS(w)
+    count += 1
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int,input().split())
     visit = [0]*(N+1)
-    G = [0]*(N+1)
+    G = [[] for _ in range(N+1)]
+    count = 0
     for _ in range(M):
         v, w = map(int,input().split())
         G[v].append(w)
-    print(G)
+        G[w].append(v)
+    for i in range(1, M+1):
+        DFS(i)
